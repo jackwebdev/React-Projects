6d6a9adf36f00876d7f0a062642c904a8cd0d1e3
Jack <33595694+jackwebdev@users.noreply.github.com>
2021-02-13 19:32:38 +0000

    Following tutorial changes

--- react-advanced-2020-master/src/App.js
+++ react-advanced-2020-master/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
+
+// UseState Imports
+// import Setup from './tutorial/1-useState/setup/1-error-example';
+// import Setup from './tutorial/1-useState/setup/2-useState-basics';
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
+
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      {/* <h2>Advanced Tutorial</h2> */}
+      <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
