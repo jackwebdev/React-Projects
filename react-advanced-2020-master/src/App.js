46a216f9027ec42825e1181de0a19bd7a21f0637
Jack <33595694+jackwebdev@users.noreply.github.com>
2021-02-20 15:43:56 +0000

    Following tutorial

--- react-advanced-2020-master/src/App.js
+++ react-advanced-2020-master/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 // UseState Imports
-// import Setup from './tutorial/1-useState/setup/1-error-example';
-// import Setup from './tutorial/1-useState/setup/2-useState-basics';
+// import Setup from "./tutorial/1-useState/setup/1-error-example";
+// import Setup from "./tutorial/1-useState/setup/2-useState-basics";
 // import Setup from "./tutorial/1-useState/setup/3-useState-array";
 // import Setup from "./tutorial/1-useState/setup/4-useState-object";
 // import Setup from "./tutorial/1-useState/setup/5-useState-counter";
@@ -13,20 +13,28 @@ import React from "react";
 // import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 
 // Conditional Rendering
-// import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
-// import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit';
-// import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+// import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+// import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
+// import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
 
 // Forms Imports
-// import Setup from './tutorial/4-forms/setup/1-controlled-inputs';
+// import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
 // import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
 
 // useRef Imports
 // import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
 
 // useReducer Imports
-import Setup from "./tutorial/6-useReducer/setup/index";
+// import Setup from "./tutorial/6-useReducer/setup/index";
 
+// Prop Dilling Imports
+// import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
+
+// useContext Imports
+// import Setup from "./tutorial/8-useContext/setup/1-context-api";
+
+// Custom Hooks Imports
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 
 function App() {
   return (
